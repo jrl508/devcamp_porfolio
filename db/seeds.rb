e88f7efa95a1bd421d6be04dbdcc816f59e2f126
Ricardo <jrl47@yahoo.com>
2017-08-20 00:32:31 -0400

    implemented custom scopes for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,18 @@
+3.times do |topic|
+    Topic.create!(
+        title: "Topic #{topic}"
+        )
+end
+
+
+
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
-    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+      topic_id: Topic.last.id   
     )
-
 end
 
 puts "10 blog posts created"
@@ -17,10 +26,20 @@ end
 
 puts "5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio title: #{portfolio_item}" ,
-    subtitle: "My great service",
+    subtitle: "Ruby on Rails",
+    body:"Lorem ipsum dolor sit amet, consectetur adipiscing " ,
+    main_image: "http://via.placeholder.com/600x400",
+    thumb_image: "http://via.placeholder.com/400x200"
+    )
+end
+
+1.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}" ,
+    subtitle: "Angular",
     body:"Lorem ipsum dolor sit amet, consectetur adipiscing " ,
     main_image: "http://via.placeholder.com/600x400",
     thumb_image: "http://via.placeholder.com/400x200"
