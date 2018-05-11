ae4456f7e8dc043e25ffcdee2619fee1638bbd09
jrl508 <jrl47@yahoo.com>
2018-05-11 11:55:48 -0400

    Implemented refactor for nav and active statuses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,4 +21,46 @@ module ApplicationHelper
         JlaViewTool::Renderer.copyright 'Jean Lafontant', 'All rights reserved'
   end
 
+  def nav_items 
+    [
+        {
+          url: root_path,
+          title: 'Home'
+        },
+        {
+          url: about_me_path, 
+          title: 'About'
+        },
+        {
+          url: contact_path, 
+          title: 'Contact'
+        },
+        {
+          url: blogs_path, 
+          title: 'Blog'
+        },
+        {
+          url: portfolios_path, 
+          title: 'Portfolio'
+        },
+
+  ]
+  end
+
+  def nav_helper tag_type, style
+
+      nav_links = ''
+
+      nav_items.each do |item|
+
+        nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"  
+      end
+
+      nav_links.html_safe
+  end
+
+  def active? path
+    'active' if current_page? path
+  end
+
 end
