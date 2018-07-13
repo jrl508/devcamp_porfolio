2312f4a2aee5876241734b8537afb3853a01a7e9
jrl508 <jrl47@yahoo.com>
2018-07-13 14:45:34 -0400

    completed comment feature with actioncable

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -21,7 +21,7 @@ module ApplicationCable
     protected
 
     def find_verified_user
-      if verified_user =env['warden'].user
+      if verified_user = env['warden'].user
         verified_user
       end
     end
